d3a39ee983731a397c81659cd7dfa70895d81aec
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-24 18:04:24 +0800

    Add files via upload

---
+++ EmpApp.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, request,url_for,flash,redirect
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+bucket = custombucket
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'employee'
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('HomePage.html')
+
+
+@app.route("/about", methods=['POST'])
+def about():
+    return render_template('www.intellipaat.com')
+
+
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    email = request.form['email']
+    emp_image_file = request.files['emp_image_file']
+    resume_image_file = request.files['resume_image_file']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s,%s)"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    if resume_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location,email))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        resume_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3,
+                resume_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    # Not relevant to our design
+    print("all modification done...")
+    return render_template('employeeDocumentation.html', name=emp_name)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+# @app.route("/addemp", methods=['GET'])
+# def AddEmp():
+# select_employee = """SELECT * FROM employee"""
+# cursor = conn.cursor()
+# cursor.execute(select_employee)
+# result = cursor.fetchall()
+#
+# for row in result:
+#     emp_id     = %row[0]
+#     first_name = %row[1]
+#     last_name  = %row[2]
+#     pri_skill  = %row[3]
+#     location   = %row[4]
+#     gender     = %row[5]
+#
+#     emp_name = "" + first_name + " " + last_name
+#     break
+#
+# left is html, right is python script
+#     return render_template('employeeDocumentation.html', emp_id=emp_id, emp_name=emp_name, pri_skill=pri_skill,location=location)
---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.czz0hfqvljiq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aws_user"
+customdb = "employee"
+custombucket = "jeremy-employee"
+customregion = "us-east-1"
