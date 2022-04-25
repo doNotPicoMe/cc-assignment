1ded180b403d3b754b01e6a02fe406e7dffee0dd
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-26 00:22:21 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -81,7 +81,7 @@ def add_employee_function():
 
     try:
 
-        cursor.execute(insert_sql, (emp_id, first_name, last_name,pri_skill, location,job,department,gender,email,salary,hire_date,age))
+        cursor.execute(insert_sql, (emp_id,first_name,last_name,pri_skill,location,job,department,gender,email,salary,hire_date,age))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
