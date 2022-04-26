5a5e036e1d7901153b7d0cf62a3dd5c575b1e796
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-27 00:12:41 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -174,7 +174,6 @@ def edit_profile_function():
 
 @app.route("/update_employee_function", methods=['POST'])
 def update_employee_function():
-    emp_id_new = request.form['emp_id_new']
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
@@ -190,7 +189,7 @@ def update_employee_function():
 
     emp_image_file = request.files['emp_image_file']
 
-    update_sql= "UPDATE employee SET emp_id=emp_id_new, first_name = first_name, last_name = last_name, age = age, gender = gender, location = location, pri_skill= pri_skill, email = email, department = department, job=job, salary =salary, hire_date = hire_date WHERE emp_id = emp_id"
+    update_sql= "UPDATE employee SET first_name = first_name, last_name = last_name, age = age, gender = gender, location = location, pri_skill= pri_skill, email = email, department = department, job=job, salary =salary, hire_date = hire_date WHERE emp_id = emp_id"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
