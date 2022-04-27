f3584e306f92dc78ad5300711d6bbbb3a07d2c73
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-27 20:57:54 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -227,7 +227,7 @@ def overtime():
     cursor = db_conn.cursor()
     cursor.execute("SELECT e.emp_id,e.first_name,e.last_name,e.job,e.department,e.salary,o.payroll FROM employee e, overtime o WHERE e.emp_id=o.emp_id")
     data = cursor.fetchall()
-    dataCount = len.(data)
+    dataCount = len(data)
     if dataCount == 0:
         return render_template('Overtime.html')
     else:
