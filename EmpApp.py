0a33cbb579e42f072e32c259a194608ecb73f18a
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-28 09:30:11 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -262,16 +262,14 @@ def delete_overtime_function():
         records = cursor.fetchall()
         for row in records:
             salary= row[0]
-
     # update_sql= 'UPDATE employee SET first_name = "first_name", last_name = "last_name", age = "age", gender = "gender", location = "location", pri_skill= "pri_skill", email = "email", department = "department", job="job", salary ="salary", hire_date = "hire_date" WHERE emp_id = "emp_id"'
-    update_overtime_sql="UPDATE payroll SET late_hours=(%s), salary=(%s) WHERE emp_id=(%s)"
-    db_conn.commit()
+    update_sql= "UPDATE payroll SET late_hours=(%s), salary=(%s) WHERE emp_id=(%s)"
 
     if emp_image_file.filename == "":
         return "Please select a file"
 
     try:
-        cursor.execute(update_overtime_sql,(late_hours,salary,emp_id))
+        cursor.execute(update_sql,(late_hours,salary,emp_id))
         db_conn.commit()
         cursor.execute("SELECT e.*,p.* FROM employee e, payroll p WHERE e.emp_id=p.emp_id")
         data = cursor.fetchall()
