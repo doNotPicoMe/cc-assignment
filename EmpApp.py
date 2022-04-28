2362ce5b4f7ebeb29e96a625121cfa08375b7330
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-28 10:01:04 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -351,5 +351,7 @@ def delete_overtime_function():
     else:
         return render_template('Overtime.html', data=data)
 
+
+
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
