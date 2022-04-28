394fcf0e823c2b1b2c37753f02d67d3d88043276
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-28 13:51:38 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -40,6 +40,7 @@ def my_profile():
         cursor.execute("SELECT * FROM employee WHERE job='admin'")
         records = cursor.fetchall()
         for row in records:
+            emp_id= row[0]
             first_name= row[1]
             last_name = row[2]
             age= row[3]
