1698e77450c8e5f6397ab20defa3e080f81641c1
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-04-28 09:44:23 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -251,34 +251,6 @@ def overtime():
 def add_overtime():
     return render_template('AddOvertime.html')
 
-@app.route("/delete_overtime_function", methods=['GET', 'POST'])
-def delete_overtime_function():
-    emp_id=request.form['emp_id']
-    late_hours="0"
-    search_sql= "SELECT salary FROM employee WHERE emp_id = (%s)"
-    cursor = db_conn.cursor()
-    try:
-        cursor.execute(search_sql,(emp_id))
-        records = cursor.fetchall()
-        for row in records:
-            salary= row[0]
-        # update_sql= 'UPDATE employee SET first_name = "first_name", last_name = "last_name", age = "age", gender = "gender", location = "location", pri_skill= "pri_skill", email = "email", department = "department", job="job", salary ="salary", hire_date = "hire_date" WHERE emp_id = "emp_id"'
-        update_sql= "UPDATE payroll SET late_hours=(%s), salary=(%s) WHERE emp_id=(%s)"
-
-        if emp_image_file.filename == "":
-            return "Please select a file"
-
-        try:
-            cursor.execute(update_sql,(late_hours,salary,emp_id))
-            db_conn.commit()
-            cursor.execute("SELECT e.*,p.* FROM employee e, payroll p WHERE e.emp_id=p.emp_id")
-            data = cursor.fetchall()
-
-            if data == None:
-                return render_template('Overtime.html')
-            else:
-                return render_template('Overtime.html', data=data)
-
 @app.route("/add_overtime_function",methods=['GET','POST'])
 def add_overtime_function():
     emp_id= request.form['emp_id']
@@ -307,6 +279,34 @@ def add_overtime_function():
         return render_template('Overtime.html')
     else:
         return render_template('Overtime.html', data=data)
+
+@app.route("/delete_overtime_function", methods=['GET', 'POST'])
+def delete_overtime_function():
+    emp_id=request.form['emp_id']
+    late_hours="0"
+    search_sql= "SELECT salary FROM employee WHERE emp_id = (%s)"
+    cursor = db_conn.cursor()
+    try:
+        cursor.execute(search_sql,(emp_id))
+        records = cursor.fetchall()
+        for row in records:
+            salary= row[0]
+        # update_sql= 'UPDATE employee SET first_name = "first_name", last_name = "last_name", age = "age", gender = "gender", location = "location", pri_skill= "pri_skill", email = "email", department = "department", job="job", salary ="salary", hire_date = "hire_date" WHERE emp_id = "emp_id"'
+        update_sql= "UPDATE payroll SET late_hours=(%s), salary=(%s) WHERE emp_id=(%s)"
+
+        if emp_image_file.filename == "":
+            return "Please select a file"
+
+        try:
+            cursor.execute(update_sql,(late_hours,salary,emp_id))
+            db_conn.commit()
+            cursor.execute("SELECT e.*,p.* FROM employee e, payroll p WHERE e.emp_id=p.emp_id")
+            data = cursor.fetchall()
+
+            if data == None:
+                return render_template('Overtime.html')
+            else:
+                return render_template('Overtime.html', data=data)
 
 @app.route("/payroll", methods=['GET', 'POST'])
 def payroll():
