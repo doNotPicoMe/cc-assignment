8d8beb8d41db42e83437e24afcbc69722fb0b472
doNotPicoMe <99383438+doNotPicoMe@users.noreply.github.com>
2022-05-12 22:24:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.czz0hfqvljiq.us-east-1.rds.amazonaws.com"
+customhost = "cc-assignment.cge3padjnblm.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "123456789"
 customdb = "employee"
